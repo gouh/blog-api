97646be0da3244f7936e0d97ffe60c419b81809d
Hugo Hernández Valdez <hugohv10@gmail.com>
2021-11-06 00:46:55 -0600

    FIX: Factory de handler

--- src/App/Factory/Handler/HealthHandlerFactory.php
+++ src/App/Factory/Handler/HealthHandlerFactory.php
@@ -2,28 +2,13 @@
 
 namespace Gouh\BlogApi\App\Factory\Handler;
 
-use Exception;
 use Gouh\BlogApi\App\Handler\HealthHandler;
-use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\ContainerInterface;
-use Psr\Container\NotFoundExceptionInterface;
 
 class HealthHandlerFactory
 {
-    /**
-     * @throws ContainerExceptionInterface
-     * @throws NotFoundExceptionInterface
-     */
     public function __invoke(ContainerInterface $container): HealthHandler
     {
-        try {
-            $config = $container->get('config');
-            return new HealthHandler($config);
-        }catch (Exception $e) {
-            echo "<pre>";
-            print_r($e->getMessage());
-            echo "<pre>";
-            exit();
-        }
+        return new HealthHandler();
     }
 }
